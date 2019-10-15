fc2d609d7bd087c6f8c196b039208f5bcda9efc8
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-10-15 07:55:46 +0200

    Removed reviewdog

--- ansible-lint/entrypoint.sh
+++ ansible-lint/entrypoint.sh
@@ -4,8 +4,6 @@ ACTION_PLAYBOOK_NAME="${INPUT_PLAYBOOK_NAME:-playbook.yml}"
 
 cd "${GITHUB_WORKSPACE}"
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
 ACTION_PLAYBOOK_PATH="${GITHUB_WORKSPACE}/${ACTION_PLAYBOOK_NAME}"
 
 if [ ! -f "${ACTION_PLAYBOOK_PATH}" -a ! -d "${ACTION_PLAYBOOK_PATH}" ]; then
@@ -15,4 +13,4 @@ fi
 
 echo "==> Linting ${ACTION_PLAYBOOK_PATH}"
 
-ansible-lint "${ACTION_PLAYBOOK_PATH}" | reviewdog -efm="%f:%l: %m" -name="${INPUT_TOOL_NAME}" -reporter=github-check
+ansible-lint "${ACTION_PLAYBOOK_PATH}"
