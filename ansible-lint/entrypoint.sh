95991ac8384a2860e715105b0f29cc53a40230c0
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-10-16 08:51:31 +0200

    Added defaults fot toolname

--- ansible-lint/entrypoint.sh
+++ ansible-lint/entrypoint.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 ACTION_PLAYBOOK_NAME="${PLAYBOOK_NAME:-playbook.yml}"
+ACTION_TOOL_NAME="${TOOL_NAME:-ansible_lint}"
 
 ACTION_PLAYBOOK_PATH="${GITHUB_WORKSPACE}/${ACTION_PLAYBOOK_NAME}"
 
@@ -15,4 +16,4 @@ if [ ! -f "${ACTION_PLAYBOOK_PATH}" -a ! -d "${ACTION_PLAYBOOK_PATH}" ]; then
   exit 1
 fi
 
-ansible-lint ${ACTION_PLAYBOOK_PATH} | reviewdog -efm="%f:%l: %m" -name="${TOOL_NAME}" -reporter=github-pr-check
+ansible-lint ${ACTION_PLAYBOOK_PATH} | reviewdog -efm="%f:%l: %m" -name="${ACTION_TOOL_NAME}" -reporter=github-pr-check
