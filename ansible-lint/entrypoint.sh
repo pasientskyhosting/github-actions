764a4c9db6e139d004a37145bdd01873577cc710
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-10-16 08:46:10 +0200

    Added inputs as env. This will convert better when using dockerfiles

--- ansible-lint/entrypoint.sh
+++ ansible-lint/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-ACTION_PLAYBOOK_NAME="${INPUT_PLAYBOOK_NAME:-playbook.yml}"
+ACTION_PLAYBOOK_NAME="${PLAYBOOK_NAME:-playbook.yml}"
 
 ACTION_PLAYBOOK_PATH="${GITHUB_WORKSPACE}/${ACTION_PLAYBOOK_NAME}"
 
@@ -8,11 +8,11 @@ cd "${GITHUB_WORKSPACE}"
 
 echo "===> Running version: $VERSION"
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+export REVIEWDOG_GITHUB_API_TOKEN="${GITHUB_TOKEN}"
 
 if [ ! -f "${ACTION_PLAYBOOK_PATH}" -a ! -d "${ACTION_PLAYBOOK_PATH}" ]; then
   echo "Cant find '${ACTION_PLAYBOOK_PATH}"
   exit 1
 fi
 
-ansible-lint ${ACTION_PLAYBOOK_PATH} | reviewdog -efm="%f:%l: %m" -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check
+ansible-lint ${ACTION_PLAYBOOK_PATH} | reviewdog -efm="%f:%l: %m" -name="${TOOL_NAME}" -reporter=github-pr-check
