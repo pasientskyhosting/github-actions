1c384f0ef8628caabad901d1387e44281f308fe7
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-10-29 13:17:38 +0100

    Fixed execution

--- pre-commit/entrypoint.sh
+++ pre-commit/entrypoint.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-cd "$GITHUB_WORKSPACE"
+# Publish Docker Container To A Package Registry
+####################################################
 
-pwd
-
-ls -lastr
-
-which git
-which pre-commit
+# exit when any command fails
+set -e
 
 echo "===> Running version: $VERSION"
 
-/usr/local/bin/pre-commit install
+# The following environment variables will be provided by the environment automatically: GITHUB_WORKSPACE, GITHUB_REF, GITHUB_SHA
+mkdir -p $GITHUB_WORKSPACE
+cd "${GITHUB_WORKSPACE}"
 
-exec /usr/local/bin/pre-commit run -a --color always
+pre-commit install
+
+exec pre-commit run -a --color always
